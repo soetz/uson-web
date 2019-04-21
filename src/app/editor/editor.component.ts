0c5568d691fb6d91f9d833b3e4e8f03dbe99f6dd
soetz <simon.lecutiez@gmail.com>
2019-04-21 16:52:58 -0400

    Added form

--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-editor',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditorComponent implements OnInit {
 
+  noteForm = new FormGroup({
+    title: new FormControl(),
+    content: new FormControl()
+  });
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSave() {
+    //TODO save note
   }
 
 }
